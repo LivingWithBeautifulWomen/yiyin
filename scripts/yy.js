5efd94ff0603448180a8caa1fe2718de2418f300
LivingWithBeautifulWomen <QDIIQF@gmail.com>
2024-02-12 10:28:30 -0500

    submenu done

--- scripts/yy.js
+++ scripts/yy.js
@@ -2,3 +2,20 @@
 	hamburger.addEventListener("click", function(){
 		document.querySelector("body").classList.toggle("active");
 	})
+
+
+let arrow = document.querySelectorAll(".arrow");
+for (var i = 0; i < arrow.length; i++) {
+	arrow[i].addEventListener("click", (e)=>{
+	let arrowParent = e.target.parentElement.parentElement;//selecting main parent of arrow
+	arrowParent.classList.toggle("showMenu");
+	});
+}
+
+let sidebar = document.querySelector(".sidebar");
+let sidebarBtn = document.querySelector(".bx-menu");
+console.log(sidebarBtn);
+sidebarBtn.addEventListener("click", ()=>{
+	sidebar.classList.toggle("close");
+});
+	
