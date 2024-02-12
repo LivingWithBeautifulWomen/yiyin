d54423e8a47934e97c6a578fe271a303da4799df
LivingWithBeautifulWomen <QDIIQF@gmail.com>
2024-02-12 11:42:14 -0500

    content1

--- scripts/yy.js
+++ scripts/yy.js
@@ -19,3 +19,23 @@ sidebarBtn.addEventListener("click", ()=>{
 	sidebar.classList.toggle("close");
 });
 	
+
+document.addEventListener('DOMContentLoaded', function() {
+    const sidebarLinks = document.querySelectorAll('.sidebar-link');
+  
+    sidebarLinks.forEach(function(link) {
+      link.addEventListener('click', function(event) {
+        event.preventDefault();
+        const contentId = this.getAttribute('data-content');
+        const contents = document.querySelectorAll('.content');
+  
+        contents.forEach(function(content) {
+          content.classList.remove('active');
+        });
+  
+        document.getElementById(contentId).classList.add('active');
+      });
+    });
+  });
+  
+  
