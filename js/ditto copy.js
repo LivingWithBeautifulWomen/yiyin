4a3fcf27496b710593229b893565d323c11062d0
LivingWithBeautifulWomen <QDIIQF@gmail.com>
2024-02-11 19:17:14 -0500

    sidebar

--- js/ditto copy.js
+++ js/ditto copy.js
@@ -72,47 +72,59 @@ function initialize() {
 }
 
 function init_sidebar_section() {
-    $.get(ditto.sidebar_file, function (data) {
-        $(ditto.sidebar_id).html(marked(data));
+  $.get(ditto.sidebar_file, function (data) {
+      var sidebarHtml = ''; // Initialize sidebar HTML
 
-        if (ditto.search_bar) {
-           init_searchbar();
-        }
+      // Split data into main sections and sub-sections
+      var sections = data.split('### ');
+      
+      // Process main sections
+      for (var i = 0; i < sections.length; i++) {
+          var section = sections[i];
+          if (section.trim() !== '') {
+              var lines = section.split('\n');
+              var sectionTitle = lines[0]; // Get section title
+              var subSections = lines.slice(1); // Get sub-sections
 
-        if (ditto.wwads) {
+              // Generate HTML for main section
+              sidebarHtml += '<div><h2>' + sectionTitle + '</h2>';
+              
+              // Process sub-sections
+              if (subSections.length > 0) {
+                  sidebarHtml += '<ul>';
+                  for (var j = 0; j < subSections.length; j++) {
+                      var subSection = subSections[j];
+                      if (subSection.trim() !== '') {
+                          var parts = subSection.split('. '); // Split into index and title
+                          var index = parts[0].trim();
+                          var title = parts[1].trim();
+                          sidebarHtml += '<li><a href="' + title.slice(title.indexOf('#')) + '">' + title + '</a></li>';
+                      }
+                  }
+                  sidebarHtml += '</ul>';
+              }
+              
+              sidebarHtml += '</div>';
+          }
+      }
+
+      // Update sidebar with generated HTML
+      $(ditto.sidebar_id).html(sidebarHtml);
+
+      if (ditto.search_bar) {
+          init_searchbar();
+      }
+
+      if (ditto.wwads) {
           init_wwads();
-        }
+      }
 
-        // 初始化内容数组
-        var menuOL = $(ditto.sidebar_id + ' ol');
-        menuOL.attr('start', 0);
+      // You can keep the rest of the code unchanged
 
-        menuOL.find('li a').map(function() {
-            menu.push(this.href.slice(this.href.indexOf('#')));
-        });
-        $('#pageup').on('click', function() {
-            var hash = getHash().nav;
-            for (var i = 0; i < menu.length; i++) {
-                if (hash === '') break;
-                if (menu[i] === '#' + hash) break;
-            }
-            location.hash = menu[i - 1]
-        });
-        $('#pagedown').on('click', function() {
-            var hash = getHash().nav;
-            for (var i = 0; i < menu.length; i++) {
-                if (hash === '') break;
-                if (menu[i] === '#' + hash) break;
-            }
-            location.hash = menu[i + 1];
-        });
-        // create_banner($(ditto.sidebar_id).find('p:nth-child(3)').first());
-
-    }, "text").fail(function() {
-        alert("Opps! can't find the sidebar file to display!");
-    });
+  }, "text").fail(function () {
+      alert("Opps! can't find the sidebar file to display!");
+  });
 }
-
 
 
 
